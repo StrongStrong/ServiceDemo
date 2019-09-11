e9456f2bb70baa5395bfb09269c0169850a473fd
雨过天晴 <91aqq@sina.com>
2019-09-11 14:13:21 +0800

    ServiceDemo

--- app/src/main/java/com/example/servicedemo/MyIntentService.java
+++ app/src/main/java/com/example/servicedemo/MyIntentService.java
@@ -1,91 +1,71 @@
 package com.example.servicedemo;
 
 import android.app.IntentService;
-import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
+
 public class MyIntentService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.testservice.action.FOO";
-    private static final String ACTION_BAZ = "com.example.testservice.action.BAZ";
+    private String TAG="IntentService";
+    /**
+     * 是否正在运行
+     */
+    private boolean isRunning;
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.testservice.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.testservice.extra.PARAM2";
+    /**
+     *进度
+     */
+    private int count;
+
+    /**
+     * 广播
+     */
 
     public MyIntentService() {
         super("MyIntentService");
+        Log.i(TAG,"MyIntentService");
     }
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG,"onCreate");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+        Log.i(TAG,"onHandleIntent");
+        try {
+            Thread.sleep(1000);
+            isRunning = true;
+            count = 0;
+            while (isRunning) {
+                count++;
+                if (count >= 100) {
+                    isRunning = false;
+                }
+                Thread.sleep(50);
+                sendThreadStatus("线程运行中...", count);
             }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
+     * 发送进度消息
      */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+    private void sendThreadStatus(String status, int progress) {
+        Intent intent = new Intent(IntentServiceActivity.ACTION_TYPE_THREAD);
+        intent.putExtra("status", status);
+        intent.putExtra("progress", progress);
+        sendBroadcast(intent);
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"线程结束运行..." + count);
     }
 }
